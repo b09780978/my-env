a80919fe865f0a9421b89a334a65eb592a05d23d
faker <b09780978@gmail.com>
2019-12-02 23:19:23 +0800

    first complete

--- README.md
+++ README.md
@@ -2,12 +2,7 @@
 my linux environment setting
 
 ## Usage
-> without docker
-> sudo bash install.sh ${USER}
-> with docker
-> sudo bash install.sh ${USER} -d
+./install.sh
 
-require: vim-plug
-install vim-plug:
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+## note
+use vim, not neovim
--- install.sh
+++ install.sh
@@ -1,14 +1,19 @@
 # Install python packages
+echo "Upgrade pip and install require python packages"
 pip2 install -U pip
 pip3 install -U pip
+pip2 install requests pyquery beautifulsoup4 ipython aiohttp --user
+pip3 install requests pyquery beautifulsoup4 ipython aiohttp --user
 
-# Install vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# Install tpm
+echo "Install tmux plugin tpm"
+git clone --depth=1 https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
 
 # Install oh-my=zsh and plugins
+echo "Install oh-my-zsh and oh-my-zsh plugins"
 git clone --depth=1 https://github.com/ohmyzsh/oh-my-zsh.git ~/.oh-my-zsh
-git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone --depth=1 https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-completions.git ~/.oh-my-zsh/custom/plugins/zsh-completions
 
 echo "Add config file"
 git clone --depth 1 https://github.com/b09780978/my-env.git ~/my-env
@@ -16,5 +21,9 @@ cp ~/my-env/.bashrc ~/.bashrc
 cp ~/my-env/.vimrc ~/.vimrc
 cp ~/my-env/.zshrc ~/.zshrc
 cp ~/my-env/.tmux.conf ~/.tmux.conf
+
+echo "Install vim plugins"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +q +UpdateRemotePlugins +q
 
 chsh -s /bin/zsh
