da65ab2c905e8c01bc7f20ee8ba12390131f0a46
b09780978 <b09780978@gmail.com>
2022-04-24 23:48:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,11 +9,13 @@ pip3 install requests pyquery beautifulsoup4 ipython aiohttp --user
 echo "Install tmux plugin tpm"
 git clone --depth=1 https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
 
-# Install oh-my=zsh and plugins
+# Install zinit and plugins
 echo "Install oh-my-zsh and oh-my-zsh plugins"
-git clone --depth=1 https://github.com/ohmyzsh/oh-my-zsh.git ~/.oh-my-zsh
-git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone --depth=1 https://github.com/zsh-users/zsh-completions.git ~/.oh-my-zsh/custom/plugins/zsh-completions
+wget "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS NF Regular.ttf"
+mkdir -p ~/.fonts/
+mv "MesloLGS NF Regular.ttf" ~/.fonts/
+zsh -ic "source ~/.zshrc"
+
 
 echo "Add config file"
 git clone --depth 1 https://github.com/b09780978/my-env.git ~/my-env
@@ -21,6 +23,7 @@ cp ~/my-env/.bashrc ~/.bashrc
 cp ~/my-env/.vimrc ~/.vimrc
 cp ~/my-env/.zshrc ~/.zshrc
 cp ~/my-env/.tmux.conf ~/.tmux.conf
+cp ~/my-env/.p10k.zsh ~/.p10k.zsh
 
 echo "Install vim plugins"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
